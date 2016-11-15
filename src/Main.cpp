d6b36d7fb0f4abc66ce01704d9ab38e3b301dfca
Jose Sánchez <erbomba@gmail.com>
2016-11-15 08:10:27 +0200

    Main file is now working with wifi as well

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include "dht_sensor.h"
-#include <WiFiConnector.h>
+#include "wifiManager.h"
 
 #define SECONDS 1000
 
@@ -9,36 +9,19 @@
   #define WIFI_PASS "8D5Y9MD306L"
 #endif
 
-WiFiConnector wifi(WIFI_SSID, WIFI_PASS);
-//WifiController mWifi;
 DHT mDHT11;
+WifiManager mWifi;
 int temperature;
 int humidity;
 bool initialised = false;
 
 void setupVar() {
   Serial.println("setupVar");
+  mWifi = WifiManager(WIFI_SSID, WIFI_PASS);
   mDHT11 = DHT();
-  WiFi.disconnect(true);
   temperature = -1;
   humidity = -1;
   initialised = true;
-}
-
-void init_wifi() {
-  wifi.init();
-  wifi.on_connected([&](const void* message)
-  {
-    Serial.print("WIFI CONNECTED WITH IP: ");
-    Serial.println(WiFi.localIP());
-  });
-
-  wifi.on_connecting([&](const void* message)
-  {
-    Serial.print("Connecting to ");
-    Serial.println(wifi.get("ssid") + ", " + wifi.get("password"));
-    delay(200);
-  });
 }
 
 void setup() {
@@ -46,16 +29,14 @@ void setup() {
   delay(5*SECONDS);
   if (!initialised) {
     setupVar();
-    init_wifi();
-    wifi.connect();
   }
   Serial.println("Main setup");
 }
 
 int readDHT11() {
   if (mDHT11.getData() == 0) {
-    temperature = mDHT11.getHumidity();
-    humidity = mDHT11.getTemperature();
+    humidity  = mDHT11.getHumidity();
+    temperature = mDHT11.getTemperature();
     return 0;
   }
   return -1;
@@ -64,14 +45,13 @@ int readDHT11() {
 void loop() {
   Serial.println("Main loop");
   if (initialised) {
-    mDHT11.loop();
-    wifi.loop();
     if (readDHT11() == 0) {
       Serial.print("Temperature: ");
       Serial.println(temperature);
       Serial.print("Humidity: ");
       Serial.println(humidity);
     }
+    mWifi.loop();
   }
   else {
     Serial.println("Setup not initialised");
